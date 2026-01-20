5460596e606d5f72dfe5a16c2c882495af32a739
Jarett Forzano <jarett@vals.ai>
2026-01-19 19:21:01 -0500

    make name better

--- services/tracker/tests/unit/conftest.py
+++ services/tracker/tests/unit/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def set_unit_test_env(monkeypatch: pytest.MonkeyPatch):
+def unit_test_environment(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setenv("DAYTONA_API_KEY", "test_key")
     monkeypatch.setenv("DAYTONA_API_URL", "http://test.url")
     monkeypatch.setenv("DAYTONA_TARGET", "test_target")
